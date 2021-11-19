67edc7c925c801ecced6a07ac4910ad76c381efa
customcommander <hello@spinjs.com>
2021-11-19 15:00:12 +0000

    test: introduce small dsl for testing

--- test.js
+++ test.js
@@ -1,47 +1,63 @@
 const test = require('tape');
 const {grammar, semantics} = require('.');
 
-const verify = (outline, input, output) => {
-  test(outline, t => {
-    const res = grammar.match(input);
-    t.true(res.succeeded(), 'Parser');
-    t.same(semantics(res).eval(), output, 'Semantics');
-    t.end();
-  });
-};
+const verify = (outline) => ({
+  parse(bibtex) {
+    return {
+      expect(output) {
+        test(outline, t => {
+          const res = grammar.match(bibtex);
+          t.true(res.succeeded(), 'Parser');
+          t.same(semantics(res).eval(), output, 'Semantics');
+          t.end();
+        });
+      }
+    }
+  }
+});
 
-verify('Supports multiple entries'
-  , `@misc{}
-     @Book {}
-     @CHAPTER {
-     }`
-  , [ {entry: 'item', type:    'misc'}
+verify('Supports multiple entries')
+  .parse(`
+    @misc{}
+    @Book {}
+    @CHAPTER {
+    }
+  `)
+  .expect([
+      {entry: 'item', type:    'misc'}
     , {entry: 'item', type:    'book'}
-    , {entry: 'item', type: 'chapter'} ]
-);
+    , {entry: 'item', type: 'chapter'}
+  ]);
 
-verify('Supports @string'
-  , `@string {}
-     @String{}
-     @STRING{
-     }
-     @STRING {
-     }`
-  , [ {entry: 'string'}
+verify('Supports @string')
+  .parse(`
+    @string {}
+    @String{}
+    @STRING{
+    }
+    @STRING {
+    }
+  `)
+  .expect([
+      {entry: 'string'}
     , {entry: 'string'}
     , {entry: 'string'}
-    , {entry: 'string'} ]
-);
+    , {entry: 'string'}
+  ]);
 
-verify('Supports @preamble'
-  , `@preamble {}
-     @Preamble{}
-     @PREAMBLE{
-     }
-     @PREAMBLE {
-     }`
-  , [ {entry: 'preamble'}
+
+verify('Supports @preamble')
+  .parse(`
+    @preamble {}
+    @Preamble{}
+    @PREAMBLE{
+    }
+    @PREAMBLE {
+    }
+  `)
+  .expect([
+      {entry: 'preamble'}
     , {entry: 'preamble'}
     , {entry: 'preamble'}
-    , {entry: 'preamble'} ]
-);
+    , {entry: 'preamble'}
+  ]);
